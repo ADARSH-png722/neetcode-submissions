d21180efbc8f0692bfa35b14ac9b0b356ad3d5ce
ADARSH KUMAR <adarshkumarhariom@gmail.com>
2026-08-15 05:01:56 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String ans = strs[0];
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        for(int i = 0 ;i<strs.length;i++){
+            while(!strs[i].startsWith(ans)){
+                ans = ans.substring(0,ans.length() -1);
+            if(ans.isEmpty()){return "";}
+            }
+        }
+        
+        return ans;
+    }
+}
